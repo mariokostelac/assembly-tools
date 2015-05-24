f725b3c5db03268aebf1519ad6e912b84733f0fe
Mario Kostelac <mario.kostelac@gmail.com>
2015-05-24 23:55:05 +0200

    isolate is_contained method

--- src/filter-contained/filter_contained.cpp
+++ src/filter-contained/filter_contained.cpp
@@ -2,40 +2,37 @@
 #include "afgreader/src/reader.h"
 
 #include <iostream>
-#include <vector>
 
 using namespace std;
 using AMOS::Overlap;
 using AMOS::Reader;
 
+bool is_contained(const Overlap& o) {
+  auto read1 = o.read1;
+  auto read2 = o.read2;
+  return (o.use_start(read1) && o.use_end(read1)) ||
+    (o.use_start(read2) && o.use_end(read2));
+}
+
 int main(int argc, char **argv) {
 
-  vector<const Overlap*> overlaps;
   int read = 0;
   int skip = 0;
 
   Reader* reader = new Reader(cin);
-  Overlap* curr_overlap;
-  while ((curr_overlap = reader->next_overlap()) != nullptr) {
-    overlaps.push_back(curr_overlap);
-    read++;
-  }
 
-  cerr << "Read " << read << " overlaps\n" << endl;
-
-  for (int i = 0; i < overlaps.size(); ++i) {
-    const auto& overlap = overlaps[i];
-    auto read1 = overlap->read1;
-    auto read2 = overlap->read2;
-    if ((overlap->use_start(read1) && overlap->use_end(read1)) ||
-        (overlap->use_start(read2) && overlap->use_end(read2))) {
+  Overlap* overlap;
+  while ((overlap = reader->next_overlap()) != nullptr) {
+    if (is_contained(*overlap)) {
       ++skip;
-      cerr << "Skipping overlap (" << read1 << ", " << read2 << ")" << endl;
+      cerr << "Skipping overlap (" << overlap->read1 << ", " << overlap->read2 << ")" << endl;
       continue;
     }
 
     cout << *overlap;
+    read++;
   }
 
+  cerr << "Read " << read << " overlaps" << endl;
   cerr << "Filtered " << skip << " overlaps" << endl;
 }
