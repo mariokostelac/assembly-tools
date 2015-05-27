d60e10d824354f4b215fd379c10216a50fd2b489
Mario Kostelac <mario.kostelac@gmail.com>
2015-05-27 14:21:58 +0200

    [filter-contained]: add filtering all overlaps of contained reads

--- src/filter-contained/filter_contained.h
+++ src/filter-contained/filter_contained.h
@@ -2,7 +2,8 @@
 #define _FILTER_CONTAINED_H
 
 #include "afgreader/src/overlap.h"
+#include <vector>
 
-bool is_contained(const AMOS::Overlap& o);
+uint32_t get_non_contained(std::vector<AMOS::Overlap*>* dst, const std::vector<AMOS::Overlap*>& overlaps);
 
 #endif
--- src/filter-contained/main.cpp
+++ src/filter-contained/main.cpp
@@ -1,38 +1,43 @@
 
 #include "filter_contained.h"
 #include "afgreader/src/reader.h"
+#include <vector>
 
+using std::vector;
 using AMOS::Reader;
 using AMOS::Overlap;
 
 int main(int argc, char **argv) {
   int read = 0;
-  int skip = 0;
 
   Reader* reader = new Reader(cin);
+  vector<Overlap*> overlaps;
 
   while (reader->has_next()) {
-    Overlap overlap;
+    Overlap* overlap = new Overlap();
     if (reader->next_type() != AMOS::OVERLAP) {
       reader->skip_next();
       continue;
     }
 
-    if (!reader->next(&overlap)) {
+    if (!reader->next(overlap)) {
       cerr << "Error while reading overlap" << endl;
       continue;
     }
 
-    if (is_contained(overlap)) {
-      ++skip;
-      cerr << "Skipping overlap (" << overlap.a_id << ", " << overlap.b_id << ")" << endl;
-      continue;
-    }
-
-    cout << overlap;
+    overlaps.emplace_back(overlap);
     read++;
   }
 
   cerr << "Read " << read << " overlaps" << endl;
-  cerr << "Filtered " << skip << " overlaps" << endl;
+
+  vector<Overlap*> filtered;
+  get_non_contained(&filtered, overlaps);
+
+  for (auto o : filtered) {
+    cout << o;
+  }
+
+  cerr << "Written " << filtered.size() << " overlaps. " << (filtered.size() / overlaps.size()) << endl;
+
 }
