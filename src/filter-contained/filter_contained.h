872f33a5171d5241ef6d7831df641b58a4a5c1ca
Mario Kostelac <mario.kostelac@gmail.com>
2015-05-27 18:59:23 +0200

    docs

--- src/filter-contained/filter_contained.h
+++ src/filter-contained/filter_contained.h
@@ -4,6 +4,7 @@
 #include "afgreader/src/overlap.h"
 #include <vector>
 
+// fills dst vector with overlaps conincident with reads that are not contained in any read from a given set.
 uint32_t get_non_contained(std::vector<AMOS::Overlap*>* dst, const std::vector<AMOS::Overlap*>& overlaps);
 
 #endif
--- src/filter-transitive/filter_transitive.h
+++ src/filter-transitive/filter_transitive.h
@@ -10,8 +10,10 @@ using std::vector;
 using AMOS::Overlap;
 
 // tests if o1 is transitive edge considering o2 and o3.
+// overlaps: {(A, B), (B, C), (A, C)}, (A, C) has a chance to be transitive.
 bool is_transitive(const Overlap& o1, const Overlap& o2, const Overlap& o3);
 
+// fills dst_container with non-transitive reads.
 int get_non_transitives(vector<shared_ptr<AMOS::Overlap>>* dst_container, vector<shared_ptr<Overlap>>& edges_list);
 
 #endif
