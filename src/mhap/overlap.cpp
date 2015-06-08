cfcbd707ec1a92d14f46b716142318afa46d4f9d
Mario Kostelac <mario.kostelac@gmail.com>
2015-06-08 14:00:55 +0200

    mhap: fix use_suffix and use_prefix functions for rc reads

--- src/mhap/overlap.cpp
+++ src/mhap/overlap.cpp
@@ -12,9 +12,17 @@ namespace MHAP {
 
   bool Overlap::use_prefix(uint32_t read_id) const {
     if (read_id == a_id) {
-      return a_lo == 0;
+      if (a_fwd) {
+        return a_lo == 0;
+      } else {
+        return a_hi == a_len;
+      }
     } else if (read_id == b_id) {
-      return b_lo == 0;
+      if (b_fwd) {
+        return b_lo == 0;
+      } else {
+        return b_hi == b_len;
+      }
     }
 
     return false;
@@ -22,9 +30,17 @@ namespace MHAP {
 
   bool Overlap::use_suffix(uint32_t read_id) const {
     if (read_id == a_id) {
-      return a_hi == a_len;
+      if (a_fwd) {
+        return a_hi == a_len;
+      } else {
+        return a_lo == 0;
+      }
     } else if (read_id == b_id) {
-      return b_hi == b_len;
+      if (a_fwd) {
+        return b_hi == b_len;
+      } else {
+        return b_lo == 0;
+      }
     }
 
     return false;
