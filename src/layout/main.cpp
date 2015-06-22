1e938cca0d5120f93394ef26e1dcaa2ffdf95597
Mario Kostelac <mario.kostelac@gmail.com>
2015-06-22 14:43:24 +0200

    add bp and trimming params

--- src/layout/main.cpp
+++ src/layout/main.cpp
@@ -8,6 +8,18 @@
 #include <iostream>
 #include <vector>
 
+// trimming params
+int READ_LEN_THRESHOLD = 100000;
+
+// BFS params in bubble popping
+size_t MAX_NODES = 750;
+int MAX_DISTANCE = 2500;
+double MAX_DIFFERENCE = 0.05;
+
+// contig extraction params
+size_t MAX_BRANCHES = 12;
+size_t MAX_START_NODES = 24;
+
 using std::cerr;
 using std::cin;
 using std::cout;
